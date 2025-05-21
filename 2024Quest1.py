c53f3498fb2f399b3ba83ea011af4d848ba4cbe2
HenryChinask1 <jeffbaier72@yahoo.com>
2025-05-20 22:42:11 -0400

    First Commit

---
+++ 2024Quest1.py
@@ -0,0 +1,41 @@
+with open('EverybodyCodes/Inputs/2024Quest1Part1.txt') as f:
+    lines = f.read()
+with open('EverybodyCodes/Inputs/2024Quest1Part2.txt') as f:
+    lines2 = f.read()
+
+def partOne():
+    potions = 0
+
+    for i in lines:
+        if i == 'B':
+            potions += 1
+        elif i == 'C':
+            potions += 3
+
+    print(f'Part One: {potions}')
+
+def partTwo():
+    potions = 0
+
+    monsterGroups = [lines2[i:i + 2] for i in range(0, len(lines2), 2)]
+
+    for i in monsterGroups:
+        if 'x' not in i:
+            potions += 2
+        if i[0] == 'B':
+            potions += 1
+        if i[1] == 'B':
+            potions += 1
+        if i[0] == 'C':
+            potions += 3
+        if i[1] == 'C':
+            potions += 3
+        if i[0] == 'D':
+            potions += 5
+        if i[1] == 'D':
+            potions += 5
+        
+    print(f'Part Two: {potions}')
+
+partOne()
+partTwo()
